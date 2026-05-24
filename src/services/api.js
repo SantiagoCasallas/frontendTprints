fa8c1bbbe1f438989df6a9a5ea7ce27ff4f0934c
SantiagoCasallas <dscasallasr@udistrital.edu.co>
2026-05-24 17:01:51 -0500

    vercel

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 // conexion a ngrok
 const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
-
 export const TOKEN_KEY = "tprints-token";
 export const USER_KEY = "tprints-user";
 
